74402806a3a5c64e14f870918118e79242bc7cbe
Alessio Fimognari <alessiofimognari@AML0220.local>
2020-01-28 16:36:20 +0000

    trying agtain

--- functions/search.js
+++ functions/search.js
@@ -4,24 +4,24 @@ exports.handler = async (event, context, callback) => {
   const confluenceAuthStr =
     'Basic YWxlc3Npby5maW1vZ25hcmlAYW1pZG8uY29tOmZjMVBFc05CTndJRUc3MnlmRlJHMjU0Ng=='
   const searchTerm = event.queryStringParameters.term
-  axios
-    .get(`https://amidodevelopment.atlassian.net/wiki/rest/api/search?cql=text~${searchTerm}`, {
+  const res = await axios.get(
+    `https://amidodevelopment.atlassian.net/wiki/rest/api/search?cql=text~${searchTerm}`,
+    {
       headers: {
         Authorization: confluenceAuthStr,
         'X-Atlassian-Token': 'no-check',
       },
-    })
-    .then(response => {
-        callback(null, {
-            statusCode: 200,
-            body: JSON.stringify({
-              status: 'SUCCESS',
-              message: '',
-              data: response.data
-            }),
-          });
-    })
-    .catch(error => {
-      console.log(error)
-    })
+    }
+  )
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      status: 'SUCCESS',
+      message: 'this is a message',
+      data: res.data,
+    }),
+  })
 }
