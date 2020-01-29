32cca7b829db027a151690d4a99fcfe33f222618
Alessio Fimognari <alessiofimognari@AML0220.local>
2020-01-29 15:16:18 +0000

    changed funciton for quote

--- functions/quote.js
+++ functions/quote.js
@@ -1,13 +1,11 @@
 const axios = require('axios')
 
 exports.handler = async (event, context, callback) => {
-  const token =
-    'Token 19c1db548f6878076281957fa6fa1c668dc0726f'
   const res = await axios.get(
     `https://api.paperquotes.com/apiv1/qod/?lang=en/`,
     {
       headers: {
-        Authorization: token,
+        Authorization: 'Token 19c1db548f6878076281957fa6fa1c668dc0726f',
         'Content-Type': 'application/json',
       },
     }
