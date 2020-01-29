fe843b1a8b2b1f7388eea6838129673e659e22da
Alessio Fimognari <alessiofimognari@AML0220.local>
2020-01-29 17:02:37 +0000

    replacing local with sessionStorage

--- src/utils/cache.tsx
+++ src/utils/cache.tsx
@@ -1,6 +1,6 @@
 export const getDataFromCache = (key: string) => {
   console.log('accessing local storage')
-  const localStorageData = localStorage.getItem(key)
+  const localStorageData = sessionStorage.getItem(key)
   if (localStorageData) {
     const expiryDate = JSON.parse(localStorageData).expiry
     if (new Date() < expiryDate) {
@@ -12,5 +12,5 @@ export const getDataFromCache = (key: string) => {
 
 export const updateCache = (key: string, data: any) => {
   console.log('writing on local storage')
-  localStorage.setItem(key, JSON.stringify({ data, expiry: new Date().setHours(24, 0, 30, 0) }))
+  sessionStorage.setItem(key, JSON.stringify({ data, expiry: new Date().setHours(24, 0, 30, 0) }))
 }
