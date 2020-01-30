49abd426dbd0936a340e0a6148885d3c168693b4
Alessio Fimognari <alessiofimognari@AML0220.local>
2020-01-30 17:16:28 +0000

    limiting CORS

--- functions/search.js
+++ functions/search.js
@@ -1,6 +1,11 @@
 const axios = require('axios')
 
 exports.handler = async (event, context, callback) => {
+  const headers = {
+    'Access-Control-Allow-Origin': 'https://amido.netlify.com',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'GET',
+  }
   const confluenceAuthStr =
     'Basic YWxlc3Npby5maW1vZ25hcmlAYW1pZG8uY29tOmZjMVBFc05CTndJRUc3MnlmRlJHMjU0Ng=='
   const searchTerm = event.queryStringParameters.term
@@ -17,6 +22,7 @@ exports.handler = async (event, context, callback) => {
     statusCode: 200,
     headers: {
       'Content-Type': 'application/json',
+      ...headers,
     },
     body: JSON.stringify(res.data),
   })
