eb2052d0bef05a48cd792e548b690a4b20c73a1e
Alessio Fimognari <alessiofimognari@AML0220.local>
2020-01-30 21:17:43 +0000

    limiting traffic to functions

--- functions/search.js
+++ functions/search.js
@@ -1,30 +1,29 @@
 const axios = require('axios')
+const REFERER = 'https://amido.netlify.com/'
 
 exports.handler = async (event, context, callback) => {
-  const headers = {
-    'Access-Control-Allow-Origin': 'https://amido.netlify.com',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Allow-Methods': 'GET',
-  }
-  const confluenceAuthStr =
-    'Basic YWxlc3Npby5maW1vZ25hcmlAYW1pZG8uY29tOmZjMVBFc05CTndJRUc3MnlmRlJHMjU0Ng=='
-  const searchTerm = event.queryStringParameters.term
-  console.log('event -> ', event)
-  const res = await axios.get(
-    `https://amidodevelopment.atlassian.net/wiki/rest/api/search?cql=text~"${searchTerm}"*`,
-    {
+  if (event.headers.referer === REFERER) {
+    const confluenceAuthStr =
+      'Basic YWxlc3Npby5maW1vZ25hcmlAYW1pZG8uY29tOmZjMVBFc05CTndJRUc3MnlmRlJHMjU0Ng=='
+    const searchTerm = event.queryStringParameters.term
+    console.log('event -> ', event)
+    const res = await axios.get(
+      `https://amidodevelopment.atlassian.net/wiki/rest/api/search?cql=text~"${searchTerm}"*`,
+      {
+        headers: {
+          Authorization: confluenceAuthStr,
+          'X-Atlassian-Token': 'no-check',
+        },
+      }
+    )
+    callback(null, {
+      statusCode: 200,
       headers: {
-        Authorization: confluenceAuthStr,
-        'X-Atlassian-Token': 'no-check',
+        'Content-Type': 'application/json',
       },
-    }
-  )
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-      ...headers,
-    },
-    body: JSON.stringify(res.data),
-  })
+      body: JSON.stringify(res.data),
+    })
+  } else {
+    callback(null, { statusCode: 401 })
+  }
 }
--- functions/searchbylabel.js
+++ functions/searchbylabel.js
@@ -1,22 +1,29 @@
 const axios = require('axios')
+const REFERER = 'https://amido.netlify.com/'
 
 exports.handler = async (event, context, callback) => {
-  const confluenceAuthStr =
-    'Basic YWxlc3Npby5maW1vZ25hcmlAYW1pZG8uY29tOmZjMVBFc05CTndJRUc3MnlmRlJHMjU0Ng=='
-  const label = event.queryStringParameters.label
-  console.log('event -> ', event)
-  const res = await axios
-  .get(`https://amidodevelopment.atlassian.net/wiki/rest/api/search?cql=label="${label}"`, {
-    headers: {
-      Authorization: confluenceAuthStr,
-      'X-Atlassian-Token': 'no-check',
-    },
-  })
-  callback(null, {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(res.data),
-  })
+  if (event.headers.referer === REFERER) {
+    const confluenceAuthStr =
+      'Basic YWxlc3Npby5maW1vZ25hcmlAYW1pZG8uY29tOmZjMVBFc05CTndJRUc3MnlmRlJHMjU0Ng=='
+    const label = event.queryStringParameters.label
+    console.log('event -> ', event)
+    const res = await axios.get(
+      `https://amidodevelopment.atlassian.net/wiki/rest/api/search?cql=label="${label}"`,
+      {
+        headers: {
+          Authorization: confluenceAuthStr,
+          'X-Atlassian-Token': 'no-check',
+        },
+      }
+    )
+    callback(null, {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(res.data),
+    })
+  } else {
+    callback(null, { statusCode: 401 })
+  }
 }
